5bf565c567d5bab467d46220bf96e99e4e0c488d
Sergey2782 <msergun234@gmail.com>
2017-07-03 00:06:30 +0400

    Deleted the unnecessary part of the code

--- js/script.js
+++ js/script.js
@@ -1,41 +1,4 @@
-/*$(document).ready(function(){
 
-    var imgStar = $(".sm_image");
-    for(i = 0; i < imgStar.length; i++){
-
-	$(imgStar[i]).click(function(){
-		imageBig();
-	});
-   }
-   $(".exit").click(function(){
-		imageSm();
-	});
-
-   var imgSm = $(".sm_image");
-   			var parentImg = imgSm.parentNode;
-
-
-    function imageBig (){
-             
-   			
-   			if($(imgSm).hasClass("sm_image")) {   
-   		         $(parentImg).addClass("col-md-6");
-   		           
-   	         }
-
-       }
-
-    function imageSm (){
-
-   			var imgBig = $(".imgbig");
-   			if($(imgBig).hasClass("imgbig")) {   
-   		         $(imgBig).removeClass("imgbig"); 
-   	         }
-
-       }
-
-})
-*/
 
 window.onload = function(){
 		var items = document.querySelectorAll('.active_one');
